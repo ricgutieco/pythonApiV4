d7f3c0ef92e73f01ccb07e4c22bb07bb0b379824
ricgutieco <70611809+ricgutieco@users.noreply.github.com>
2020-10-27 08:16:10 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -7,15 +7,21 @@ class Client():
         self.last_name = last_name
         self.doc_id = doc_id
         self.preexistence = []
+        
+    """ Ads a prexistencies to a client
+    """
 
     def add_preexistence(self, n_preexistence):
         self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
+    
+    """ Ads a prexistencies to a client
+    """
 
     def get_preexistence(self, p_index):
         if p_index >= len(self.preexistence):
             return 'There is no such preexistence'
-        else:
+      
             return self.preexistence[p_index]
 
     def get_all_preexistence(self):
